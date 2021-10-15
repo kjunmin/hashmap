c405c1cccb4ec0598d1a30b7bf86921a80e642df
Matthew <kjm.matthew@gmail.com>
2021-10-15 23:29:12 +0800

    Hashmap impl

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# HashMap
+# HashMap 
+ A simple implmentation of a <b>HashMap</b> that allows dynamic types for keys and values in Golang.
+
+# Download and install
+```
+$ go get github.com/kjunmin/HashMap
+```
+
+# Usage
+```
+package main
+
+import (
+    "fmt"
+    "github.com/kjunmin/HashMap"
+)
+
+func main() {
+	hashMap := Init(64, HashFunc)
+
+	hashMap.Insert(1, "Test")
+	hashMap.Insert(2, 777)
+	hashMap.Insert("Hello", "World")
+	hashMap.Insert("Slices", []int{5, 6, 7, 8})
+	fmt.Println(hashMap.Get(1))
+	// output: Test
+	fmt.Println(hashMap.Get("Hello"))
+	// output: World
+	fmt.Println(hashMap.Get("Slices"))
+	//output: [5, 6, 7, 8]
+	fmt.Println(hashMap.Get(3))
+	// output: Key not found
+	fmt.Println(hashMap.Count())
+	// output: 4
+	hashMap.Erase("Slices")
+	fmt.Println(hashMap.Get("Slices"))
+	// output: Key not found
+	fmt.Println(hashMap.Count())
+	// output: 3
+}
+```
---
+++ hashmap.go
@@ -0,0 +1,117 @@
+package hashmap
+
+import (
+	"bytes"
+	"errors"
+	"reflect"
+)
+
+type Key interface{}
+type Value interface{}
+
+type HashMaper interface {
+	Insert(key Key, value Value) error
+	Get(key Key) (value Value, err error)
+	Erase(key Key) error
+	Count() int
+}
+
+type HashMap struct {
+	hashFunc         func(blockSize int, key Key) (hashKey uint, bucketIdx uint)
+	defaultBlockSize int
+	buckets          []*BucketNode
+	size             int
+}
+
+type BucketNode struct {
+	hashKey uint
+	key     Key
+	value   Value
+	next    *BucketNode
+}
+
+func Init(blockSize int, fn func(blockSize int, key Key) (hashKey uint, bucketIdx uint)) *HashMap {
+	hashMap := new(HashMap)
+	hashMap.hashFunc = fn
+	hashMap.defaultBlockSize = blockSize
+	hashMap.buckets = make([]*BucketNode, blockSize)
+	hashMap.size = 0
+
+	return hashMap
+}
+
+// Hashmap methods
+func (h *HashMap) Get(key Key) (value Value, err error) {
+	hashKey, bucketIdx := h.hashFunc(len(h.buckets), key)
+	bucketNode := h.buckets[bucketIdx]
+	for bucketNode != nil {
+		if bucketNode.hashKey == hashKey && reflect.DeepEqual(key, bucketNode.key) {
+			return bucketNode.value, nil
+		}
+		bucketNode = bucketNode.next
+	}
+	return nil, errors.New("Key not found")
+}
+
+func (h *HashMap) Insert(key Key, value Value) error {
+	hashKey, bucketIdx := h.hashFunc(len(h.buckets), key)
+	bucketNode := h.buckets[bucketIdx]
+	newNode := &BucketNode{
+		hashKey: hashKey,
+		key:     key,
+		value:   value,
+		next:    bucketNode,
+	}
+	h.buckets[bucketIdx] = newNode
+	h.size++
+	return nil
+}
+
+func (h *HashMap) Erase(key Key) error {
+	hashKey, bucketIdx := h.hashFunc(len(h.buckets), key)
+	bucketNode := h.buckets[bucketIdx]
+	if bucketNode == nil {
+		return errors.New("Key to delete not found")
+	}
+
+	var prev *BucketNode
+	for bucketNode != nil {
+		if bucketNode.hashKey == hashKey && reflect.DeepEqual(key, bucketNode.key) {
+			if prev == nil && bucketNode.next == nil {
+				h.buckets[bucketIdx] = nil
+			} else if prev == nil {
+				h.buckets[bucketIdx] = bucketNode.next
+			} else {
+				prev.next = bucketNode.next
+			}
+		}
+		prev = bucketNode
+		bucketNode = bucketNode.next
+	}
+	h.size--
+	return nil
+}
+
+func (h *HashMap) Count() int {
+	return h.size
+}
+
+// Hashing function
+func HashFunc(blockSize int, key Key) (hashKey uint, bucketIdx uint) {
+	var buf bytes.Buffer
+	buf.Write([]byte(reflect.ValueOf(key).String()))
+
+	hash := dbj2hash(&buf)
+
+	return hash, (hash % uint(blockSize))
+}
+
+func dbj2hash(buf *bytes.Buffer) uint {
+	var hash uint = 5381
+
+	for _, c := range buf.Bytes() {
+		hash = (hash << 5) + hash + uint(c)
+	}
+
+	return hash
+}
